123e324dade7d25cf3cc2ed57a8e996c75fb0dc4
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-22 11:09:43 +0900

    B04

--- tessoku-book/src/bin/b04.rs
+++ tessoku-book/src/bin/b04.rs
@@ -1,3 +1,14 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        mut n: i32,
+    }
+    let mut sum = 0;
+    let mut base = 1;
+    while 0 < n {
+        let value = n % 10;
+        sum += value * base;
+        n /= 10;
+        base *= 2;
+    }
+    println!("{}", sum);
 }
