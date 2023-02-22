4448f0fb87b217f40dbeeb69c538b244b41ab794
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-22 13:12:33 +0900

    A05

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,3 +1,17 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: i32,
+        k: i32,
+    }
+    let mut counter = 0;
+    // 赤色のカードを記入
+    for r in 1..=n {
+        // 青色のカードを記入
+        for b in 1..=n {
+            // 白色のカードを計算
+            let w = k - r - b;
+            counter += if 1 <= w && w <= n { 1 } else { 0 };
+        }
+    }
+    println!("{}", counter);
 }
