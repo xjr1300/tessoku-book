f82125f9c9143c284fa6384d3c2e21791d41d74e
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-22 13:28:38 +0900

    A06

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -1,3 +1,15 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        q: usize,
+        a: [i32; n],
+        lr: [(usize, usize); q],
+    }
+    let mut sum = vec![0; n + 1];
+    for i in 1..=n {
+        sum[i] = sum[i - 1] + a[i - 1];
+    }
+    for (l, r) in lr {
+        println!("{}", sum[r] - sum[l - 1]);
+    }
 }
