8f3b3ab3d2319b0026669b35743fce669bdfa8b7
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-22 09:52:05 +0900

    A03

--- tessoku-book/src/bin/a03.rs
+++ tessoku-book/src/bin/a03.rs
@@ -1,3 +1,21 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        k: i32,
+        p: [i32; n],
+        q: [i32; n],
+    }
+    let mut result = false;
+    for i in 0..n {
+        for j in 0..n {
+            if p[i] + q[j] == k {
+                result = true;
+                break;
+            }
+        }
+        if result {
+            break;
+        }
+    }
+    println!("{}", if result { "Yes" } else { "No" });
 }
