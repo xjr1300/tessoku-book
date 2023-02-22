9d56e719b0029f9b0e360c3b8be05ad5e3ba0b92
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-22 09:42:51 +0900

    A02

--- tessoku-book/src/bin/a02.rs
+++ tessoku-book/src/bin/a02.rs
@@ -1,3 +1,9 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        x: i32,
+        a: [i32; n],
+    }
+    let result = a.iter().any(|v| *v == x);
+    println!("{}", if result { "Yes" } else { "No" });
 }
