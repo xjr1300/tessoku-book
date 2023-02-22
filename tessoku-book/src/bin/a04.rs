695a9bae21a9b3abb8bb347ba2cbea9696326bc8
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-22 11:02:00 +0900

    A04

--- tessoku-book/src/bin/a04.rs
+++ tessoku-book/src/bin/a04.rs
@@ -1,3 +1,16 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        mut n: i32,
+    }
+    let mut bin = [0; 10];
+    let mut i = 9i32;
+    while 0 < n && 0 <= i {
+        bin[i as usize] = n % 2;
+        i -= 1;
+        n /= 2;
+    }
+    for i in bin.iter() {
+        print!("{}", i);
+    }
+    println!();
 }
