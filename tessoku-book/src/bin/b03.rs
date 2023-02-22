ec91f0255480a3d87c07a130651f18fc8e425473
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-22 10:11:10 +0900

    B03

--- tessoku-book/src/bin/b03.rs
+++ tessoku-book/src/bin/b03.rs
@@ -1,3 +1,12 @@
+use itertools::Itertools;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        a: [i32; n],
+    }
+    let result = (0..n)
+        .permutations(3)
+        .any(|v| a[v[0]] + a[v[1]] + a[v[2]] == 1_000);
+    println!("{}", if result { "Yes" } else { "No" });
 }
