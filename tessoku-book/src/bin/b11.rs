7df3f4b5a5b8fe86a3b800f796ebec7c028f3151
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-23 12:45:09 +0900

    B11

--- tessoku-book/src/bin/b11.rs
+++ tessoku-book/src/bin/b11.rs
@@ -1,3 +1,18 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        mut a: [i32; n],
+        q: usize,
+    }
+    a.sort();
+    for _ in 0..q {
+        proconio::input! {
+            x: i32,
+        }
+        let mut index = a.binary_search(&x).unwrap_or_else(|index| index);
+        while index > 1 && a[index - 1] == x {
+            index -= 1;
+        }
+        println!("{}", index);
+    }
 }
