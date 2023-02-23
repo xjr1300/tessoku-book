1bdddebc9a2e460f5e32688afc0b3513b466d901
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-23 10:49:57 +0900

    A10

--- tessoku-book/src/bin/a10.rs
+++ tessoku-book/src/bin/a10.rs
@@ -1,3 +1,27 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        a: [i32; n],
+        d: usize,
+        lr: [(usize, usize); d],
+    }
+    let mut forward = vec![0; n + 2];
+    for i in 1..=n {
+        if a[i - 1] < forward[i - 1] {
+            forward[i] = forward[i - 1];
+        } else {
+            forward[i] = a[i - 1];
+        }
+    }
+    let mut backward = vec![0; n + 2];
+    for i in (1..=n).rev() {
+        if a[i - 1] < backward[i + 1] {
+            backward[i] = backward[i + 1];
+        } else {
+            backward[i] = a[i - 1];
+        }
+    }
+    for (l, r) in lr {
+        println!("{}", forward[l - 1].max(backward[r + 1]));
+    }
 }
