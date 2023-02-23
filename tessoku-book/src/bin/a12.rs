f2fee88f0aa332a53526b9b7e973deb2da83cfde
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-23 14:12:26 +0900

    A12

--- tessoku-book/src/bin/a12.rs
+++ tessoku-book/src/bin/a12.rs
@@ -1,3 +1,28 @@
+fn num_of_printed(a: &[u64], seconds: u64) -> u64 {
+    let mut num = 0;
+    for x in a {
+        num += seconds / x;
+    }
+
+    num
+}
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        k: u64,
+        a: [u64; n],
+    }
+    let mut l = 1;
+    let mut r = 10u64.pow(9);
+    while l < r {
+        let i = (r + l) / 2;
+        let num = num_of_printed(&a, i);
+        if k <= num {
+            r = i;
+        } else {
+            l = i + 1;
+        }
+    }
+    println!("{}", l);
 }
