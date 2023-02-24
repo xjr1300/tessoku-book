198092d3ca74d2a90858fed4ac26467eb4b3b9c4
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-24 19:06:38 +0900

    B14

--- tessoku-book/src/bin/b14.rs
+++ tessoku-book/src/bin/b14.rs
@@ -1,3 +1,37 @@
+use itertools::Itertools;
+
+fn combination_sum(a: &[i32], l: usize, r: usize) -> Vec<i32> {
+    let mut v = vec![0];
+    for i in 1..=r - l {
+        for combi in (l..r).combinations(i) {
+            let mut sum = 0;
+            for i in combi {
+                sum += a[i];
+            }
+            v.push(sum);
+        }
+    }
+
+    v
+}
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        k: i32,
+        a: [i32; n],
+    }
+    let m = n / 2;
+    let mut p = combination_sum(&a, 0, m);
+    p.sort();
+    let mut q = combination_sum(&a, m, n);
+    q.sort();
+    let mut found = false;
+    for n in p {
+        if q.binary_search(&(k - n)).is_ok() {
+            found = true;
+            break;
+        }
+    }
+    println!("{}", if found { "Yes" } else { "No" });
 }
