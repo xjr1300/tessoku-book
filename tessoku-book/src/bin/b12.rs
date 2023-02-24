86a9f8bb0a78bc0c1782d1d6446a5a06062c50f6
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-24 13:10:25 +0900

    B12

--- tessoku-book/src/bin/b12.rs
+++ tessoku-book/src/bin/b12.rs
@@ -1,3 +1,19 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: f64,
+    }
+    let mut l = 0.0;
+    let mut r = n;
+    let mut v = 0.0;
+    let mut m = -1.0;
+    while (v - n).abs() > 1e-5 {
+        m = (r + l) / 2.0;
+        v = m * m * m + m;
+        if v < n {
+            l = m;
+        } else {
+            r = m;
+        }
+    }
+    println!("{:.6}", m);
 }
