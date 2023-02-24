e965afa7a45e37a1d41015117cadf887b84ef331
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-24 16:21:35 +0900

    B13

--- tessoku-book/src/bin/b13.rs
+++ tessoku-book/src/bin/b13.rs
@@ -1,3 +1,26 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+    // 累積和を計算
+    let mut acc = vec![0; n + 1];
+    for i in 1..=n {
+        acc[i] = acc[i - 1] + a[i - 1];
+    }
+    // 尺取法で計算
+    let mut r = vec![n; n + 1];
+    for i in 1..=n {
+        r[i] = if i == 1 { 0 } else { r[i - 1] };
+        while r[i] < n && acc[r[i] + 1] - acc[i - 1] <= k {
+            r[i] += 1;
+        }
+    }
+    // 何通り買い方があるか計算して出力
+    let mut num = 0;
+    for i in 1..=n {
+        num += r[i] + 1 - i;
+    }
+    println!("{:?}", num);
 }
