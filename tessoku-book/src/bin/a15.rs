fa144c260ca6fa01565151f22ea68e33faf34b02
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-24 19:06:47 +0900

    A15

--- tessoku-book/src/bin/a15.rs
+++ tessoku-book/src/bin/a15.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        a: [i32; n],
+    }
+    let mut b = a.clone();
+    b.sort();
+    b.dedup();
+    let mut m = HashMap::new();
+    for (i, v) in b.iter().enumerate() {
+        m.insert(v, i + 1);
+    }
+    for n in a.iter() {
+        print!("{} ", m.get(&n).unwrap());
+    }
+    println!();
 }
