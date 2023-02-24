ac6b38853681aeba66b6105acc44b1e3cfc6c294
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-24 14:05:17 +0900

    A13

--- tessoku-book/src/bin/a13.rs
+++ tessoku-book/src/bin/a13.rs
@@ -1,3 +1,23 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        k: i32,
+        a: [i32; n],
+    }
+    let mut r = vec![n - 1; n];
+    let mut i = 0;
+    let mut j = 0;
+    while i < n {
+        r[i] = j;
+        while j < n && a[j] - a[i] <= k {
+            j += 1;
+            r[i] = j;
+        }
+        i += 1;
+    }
+    let mut num = 0;
+    for i in 0..n {
+        num += r[i] - (i + 1);
+    }
+    println!("{}", num);
 }
