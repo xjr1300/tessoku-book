636fd1a456ad7097127f8e8a375bf99760d8e80b
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-27 20:32:32 +0900

    A19

--- tessoku-book/src/bin/a19.rs
+++ tessoku-book/src/bin/a19.rs
@@ -1,3 +1,22 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        w: i32,
+        wv: [(usize, i64); n],
+    }
+    let mut dp = vec![vec![std::i64::MIN; w as usize + 1]; n + 1];
+    // 何も選択しないときの価値を設定
+    dp[0][0] = 0;
+    // 品物1以降を選択するか、選択しないかでdpを設定
+    for i in 1..=n {
+        // 0からwまでループ
+        for j in 0usize..=w as usize {
+            if j < wv[i - 1].0 {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = dp[i - 1][j].max(dp[i - 1][j - wv[i - 1].0] + wv[i - 1].1);
+            }
+        }
+    }
+    println!("{}", dp[n].iter().max().unwrap());
 }
